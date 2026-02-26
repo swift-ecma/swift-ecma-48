9c80ccfa12fedb5bbccef5b7e7a62d846784f5f2
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-02-26 10:27:02 +0100

    Flatten test structure and migrate to Apple Swift Testing

--- Package.swift
+++ Package.swift
@@ -25,7 +25,13 @@ let package = Package(
             dependencies: [
                 .product(name: "Standard Library Extensions", package: "swift-standard-library-extensions")
             ]
-        )
+        ),
+        .testTarget(
+            name: "ECMA 48 Tests",
+            dependencies: [
+                "ECMA 48",
+            ]
+        ),
     ],
     swiftLanguageModes: [.v6]
 )
@@ -40,6 +46,7 @@ for target in package.targets where ![.system, .binary, .plugin, .macro].contain
         .enableUpcomingFeature("ExistentialAny"),
         .enableUpcomingFeature("InternalImportsByDefault"),
         .enableUpcomingFeature("MemberImportVisibility"),
+        .enableUpcomingFeature("NonisolatedNonsendingByDefault"),
         .enableExperimentalFeature("Lifetimes"),
         .enableExperimentalFeature("SuppressedAssociatedTypes"),
         .enableExperimentalFeature("SuppressedAssociatedTypesWithDefaults"),
